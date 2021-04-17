4a5a772288be412990287c05b00ff1d89509f5c8
Davsav <davidbyu6@gmail.com>
2021-04-16 22:46:08 -0600

    created local storage

--- Develop/script.js
+++ Develop/script.js
@@ -34,19 +34,39 @@ var ppf= function () {
 
 ppf();
 
-
-
 function persistInput() {
-    var inputText = document.getElementById("09");
-    //for( var i=0; i <)
-    
-    localStorage.setItem("text", inputText.value);
+    var inputText = document.querySelectorAll(".textarea");
+    var inputTextArr = Array.from(inputText);
+    console.log(inputTextArr);
+    inputTextArr.forEach(element => {
+        console.log(element.value)
+        localStorage.setItem(`text${element.attributes[1].value}`, JSON.stringify(element.value));
+    })
 
 };
 
 function pageLoad() {
- 
-
+    var returnedItems = [];
+    for (var i = 9; i < 21; i++) {
+        if (i === 9) {
+            returnedItems.push(window.localStorage.getItem(`text0${i}`))
+        } else {
+            returnedItems.push(window.localStorage.getItem(`text${i}`));
+        }
+    }
+        document.getElementById("09").innerHTML += JSON.parse(localStorage.getItem("text09"));
+        document.getElementById("10").innerHTML += JSON.parse(localStorage.getItem("text10"));
+        document.getElementById("11").innerHTML += JSON.parse(localStorage.getItem("text11"));
+        document.getElementById("12").innerHTML += JSON.parse(localStorage.getItem("text12"));
+        document.getElementById("13").innerHTML += JSON.parse(localStorage.getItem("text13"));
+        document.getElementById("14").innerHTML += JSON.parse(localStorage.getItem("text14"));
+        document.getElementById("15").innerHTML += JSON.parse(localStorage.getItem("text15"));
+        document.getElementById("16").innerHTML += JSON.parse(localStorage.getItem("text16"));
+        document.getElementById("17").innerHTML += JSON.parse(localStorage.getItem("text17"));
+        document.getElementById("18").innerHTML += JSON.parse(localStorage.getItem("text18"));
+        document.getElementById("19").innerHTML += JSON.parse(localStorage.getItem("text19"));
+        document.getElementById("20").innerHTML += JSON.parse(localStorage.getItem("text20"));
+    // })
 };
 
 pageLoad();
